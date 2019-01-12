b3229bb0d1a7612c1d9bab4ed3434f9b0dd0c09a
Andrew Trevers <andrew.trevers@potterclarkson.com>
2019-01-12 15:09:29 +0000

    Changed video width

--- js/main.js
+++ js/main.js
@@ -118,7 +118,7 @@
 	// When the window is resized
 	$(window).resize(function () {
 
-		var newWidth = $fluidEl.width();
+		var newWidth = $fluidEl.width() / 2.1;
 
 		// Resize all videos according to their own aspect ratio
 		$allVideos.each(function () {
